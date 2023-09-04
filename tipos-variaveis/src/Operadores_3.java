bbfec7ece6913da5084ec0152159a54675a66018
vagner <eddievagner23@yahoo.com.br>
2023-09-04 18:46:42 -0300

    operadore booleans

---
+++ tipos-variaveis/src/Operadores_3.java
@@ -0,0 +1,15 @@
+public class Operadores_3 {
+    
+    public static void main(String[] args) {
+        int numero = 5;
+        
+        //x repeticao
+        //numero++;
+        //numero = numero + 1;
+
+        //System.out.println( numero ++); //incrementa
+        System.out.println(numero --); // decrementa
+    
+        System.out.println(numero);
+    }
+}
---
+++ tipos-variaveis/src/Operadores_Boolean.java
@@ -0,0 +1,11 @@
+public class Operadores_Boolean {
+    
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+
+}
